2396a818ec71f04ad39394cb6441fecdd91c55bb
joachimbovin <joachimbovin@hotmail.com>
2023-07-10 16:56:40 +0200

    added portfolio: De tijd

---
+++ detijd/presidenten/script.js
@@ -0,0 +1,152 @@
+
+
+
+// set the dimensions and margins of the graph
+var margin = {top: 10, right: 20, bottom: 30, left: 50},
+    width = 1000 - margin.left - margin.right,
+    height = 500 - margin.top - margin.bottom;
+
+// append the svg object to the body of the page
+var svg = d3.select("#graph")
+  .append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+  .append("g")
+    .attr("transform",
+          "translate(" + margin.left + "," + margin.top + ")");
+
+
+var defs = svg.append("defs");
+
+defs.append("pattern")
+  .attr("id", "jon-snow")
+  .attr("height", "100%")
+  .attr("width", "100%")
+  .attr("patternContentUnits", "objectBoundingBox")
+  .append("image")
+  .attr("height", 1)
+  .attr("width", 1)
+  .attr("preserveAspectRatio", "none")
+  .attr("xmlns:xlink", "http://www.w3.org/1999/xlink")
+  .attr("xlink:href", "joebiden.jpg")
+
+
+
+
+
+//Read the data
+d3.csv("dataPresidentImages2.csv", function(data) {
+
+
+  defs.selectAll(".artist-pattern")
+  .data(data)
+  .enter().append("pattern")
+  .attr("class", "artist-pattern")
+  .attr("id", 
+  function(d) {
+    return d.name.toLowerCase().replace(/ /g, "-")
+  }
+  // "jon-snow"
+  )
+  .attr("height", "100%")
+  .attr("width", "100%")
+  .attr("patternContentUnits", "objectBoundingBox")
+  .append("image")
+  .attr("height", 1)
+  .attr("width", 1)
+  .attr("preserveAspectRatio", "none")
+  .attr("xmlns:xlink", "http://www.w3.org/1999/xlink")
+  .attr("xlink:href", 
+  function(d) {
+      return d.imageURL
+  }
+  )
+
+  // Add X axis
+  var x = d3.scaleLinear()
+    .domain([1750, 2050])
+    .range([ 0, width ]);
+  svg.append("g")
+    .attr("transform", "translate(0," + height + ")")
+    .call(d3.axisBottom(x));
+
+  // Add Y axis
+  var y = d3.scaleLinear()
+    .domain([35, 80])
+    .range([ height, 0]);
+  svg.append("g")
+    .call(d3.axisLeft(y));
+
+  // Add a scale for bubble size
+  var z = d3.scaleLinear()
+    .domain([10, 100])
+    .range([ 1, 40]);
+
+
+// Add a tooltip div. Here I define the general feature of the tooltip: stuff that do not depend on the data point.
+  // Its opacity is set to 0: we don't see it by default.
+  var tooltip = d3.select("#graph")
+    .append("div")
+    .style("opacity", 0)
+    .attr("class", "tooltip")
+    .style("background-color", "white")
+    .style("border", "solid")
+    .style("border-width", "1px")
+    .style("border-radius", "5px")
+    .style("padding", "10px")
+
+
+
+  // A function that change this tooltip when the user hover a point.
+  // Its opacity is set to 1: we can now see it. Plus it set the text and position of tooltip depending on the datapoint (d)
+  var mouseover = function(d) {
+    tooltip
+      .style("opacity", 1)
+      console.log("hello there")
+  }
+
+  var mousemove = function(d) {
+    tooltip
+      .html("name: " + d.name + ", age: " + d.ageStart)
+      .style("left", (d3.mouse(this)[0]+90) + "px") // It is important to put the +90: other wise the tooltip is exactly where the point is an it creates a weird effect
+      .style("top", (d3.mouse(this)[1]) + "px")
+  }
+
+  // A function that change this tooltip when the leaves a point: just need to set opacity to 0 again
+  var mouseleave = function(d) {
+    tooltip
+      .transition()
+      .duration(200)
+      .style("opacity", 0)
+  }
+
+
+  // Add dots
+  svg.append('g')
+    .selectAll("dot")
+    .data(data)
+    .enter()
+    .append("circle")
+      .attr("cx", function (d) { return x(d.dateStart); } )
+      .attr("cy", function (d) { return y(d.ageStart); } )
+      .attr("r", function (d) { return z(d.ageStart); } )
+      .style("fill",
+      
+      //"url(#jon-snow)"
+      
+ 
+      function (d) {
+        return "url(#" + d.name.toLowerCase().replace(/ /g, "-") + ")"
+      }
+
+      )
+      
+      //#69b3a2
+      .style("opacity", "0.7")
+      .attr("stroke", "black")
+      .on("mouseover", mouseover )
+      .on("mousemove", mousemove )
+      .on("mouseleave", mouseleave )
+  
+
+})
---
+++ detijd/vertaling/functions.js
@@ -0,0 +1,314 @@
+var author = "None";
+
+(function() {
+    var width = 900,
+        height = 720;
+
+
+    var brewer = d3.entries(colorbrewer)[28].value[8].reverse();  /* change the number (21) to change the color */
+
+    var svg = d3.select("#chart")
+        .append("svg")
+        .attr("style","overflow:visible")  //CSS !!!
+        .attr("height", height)
+        .attr("width", width)
+        .append("g")
+        .attr("transform", "translate(0,0)");
+
+    var radiusScale = d3.scaleSqrt().domain([1, 1500]).range([1,20]);
+
+    var div = d3.select("#chart").append("div")
+        .attr("class", "tooltip")
+        .style("opacity", 0);
+
+    // the simulation is collection of forces about
+    //where we want our circles to go
+    // and how we want our circles to interact
+    var simulation = d3.forceSimulation()
+    // force( name, define force ...)
+        .force("x", d3.forceX(width / 2).strength(0.04))
+        .force('y', d3.forceY(height / 2).strength(0.04))
+        .force("collide", d3.forceCollide(function(d)  {
+            return radiusScale(d.freq) + 2;
+        }   ))     //radius of circles has to match radius of forcecollide
+
+
+    d3.select("#literature").on("click", function() {
+        console.log("hello")
+
+    })
+
+    d3.queue()
+        .defer(d3.csv, "authors_combined_full.csv")
+        .await(ready)
+
+    function ready(error, datapoints) {
+        var circles = svg.selectAll(".full_name")
+        //console.log(datapoints)
+            .data(datapoints)
+            //.data(datapoints.filter(function(d){
+            //    return d.genre === genre;
+            //}))
+            //console.log(datapoints)
+            .enter().append("circle")
+            //.attr("style", "stroke:red")
+            .attr("id","authors")
+            .style("stroke-width", 5)
+            .attr("r", 2)
+            .attr("r", function(d) {
+                return radiusScale(d.freq)
+            })
+            .attr("fill", function(d) {
+                if (d.genre === "religion") {
+                    return brewer[1];
+                } else if (d.genre === "philosophy") {
+                    return brewer[2];
+                } else if (d.genre === "law_social_sciences_education") {
+                    return brewer[3];
+                }  else if (d.genre === "applied_science") {
+                    return "#e7298a";
+                }  else if (d.genre === "exact_science") {
+                    return "#8e8d99";
+                }  else if (d.genre === "art_games_sport") {
+                    return "#d90d02";
+                } else if (d.genre === "literature") {
+                    return brewer[5];
+                } else if (d.genre === "history_geography_biography") {
+                    return brewer[7];
+                }
+                return "#e7298a";
+            })
+            //.attr("fill", "red")
+            // .attr("cx", 100)
+            // .attr("cy", 300)
+            .on('click', function(d) {
+                document.getElementById('author').innerHTML = "Author: " + d.full_name;
+                document.getElementById('frequency').innerHTML = "Number of translated texts: " + d.freq;
+                document.getElementById('genre').innerHTML = "Genre: " + d.genre
+                get_picture(d.full_name)
+                d3.select(this).attr("style", "stroke:black");
+
+                /*            d3.select(this).attr("r", function(d) {
+                                return radiusScale(d.freq) + 20
+                            });*/
+
+            })
+            /*     .on("mouseover", function(d) {
+                     document.getElementById('author').innerHTML = d.full_name;
+                 })*/
+            .on("mouseover", function(d) {
+                div.transition()
+                    .duration(200)
+                    .style("opacity", .9);
+                div	.html(d.full_name)
+                    .style("left", (d3.event.pageX) + "px")
+                    .style("top", (d3.event.pageY - 28) + "px");
+            })
+            .on("mouseout", function(d) {
+                div.transition()
+                    .duration(500)
+                    .style("opacity", 0);
+            })
+
+
+
+
+        simulation.nodes(datapoints)
+            .on('tick', ticked)//we feed the simulation our nodes, our circles...
+
+        //we write a code that fires at every tick of the clock
+        function ticked() {
+            circles
+                .attr("cx", function (d) {
+                    return d.x
+                })
+                .attr("cy", function(d) {
+                    return d.y
+                })
+        }
+
+    }
+
+
+
+
+
+})();
+
+
+
+function get_picture(author) {
+
+    $(document).ready(function () {
+
+        get_QID(author)
+
+        //get_image()
+        //var name = $("#author");
+        //var name = author
+        //var name = d.full_name
+        //var name = document.getElementById('author').innerHTML
+    })
+}
+
+
+
+function get_QID(author) {
+
+
+    $.ajax({
+
+        url: "https://www.wikidata.org/w/api.php?action=query&list=search&srsearch=" + author + "&format=json&origin=*",
+
+        success: function (result) {
+
+            var Q_ID = result.query.search[0]["title"];   //pageid (?)
+
+
+            get_image(Q_ID)
+            get_country_id(Q_ID)
+
+
+
+        }
+
+    })
+}
+
+
+function get_image(Q_ID) {
+
+    //console.log(Q_ID)
+
+    $.ajax({
+
+        url: "https://www.wikidata.org/w/api.php?action=wbgetclaims&format=json&entity=" + Q_ID + "&origin=*",
+
+
+        success: function (result) {
+            $( "#results" ).empty();
+            var item = result.claims.P18[0].mainsnak.datavalue.value;
+            var new_item = item.split(' ').join('_');
+            var test = (MD5(new_item));
+            var number_1 = test[0];
+            var number_2 = test[0] + test[1];
+
+            //doesn't work all the time, probably an encoding (utf-8) problem e.g. Q457 > = île-de-blabla, doesn't work!
+
+            var output = "<img id='img' src='https://upload.wikimedia.org/wikipedia/commons/" + number_1 + "/" + number_2 + "/" + new_item + "'>"
+            $("#results").append(output);
+
+        },
+        error: function (result) {
+            console.log("Error: ", result)
+        }
+    })
+}
+
+
+
+function get_country_id(Q_ID) {
+
+    $.ajax({
+
+        url: "https://www.wikidata.org/w/api.php?action=wbgetclaims&format=json&entity=" + Q_ID + "&property=P27&origin=*",
+
+        success: function (result) {
+            var country_ID = result.claims.P27[0].mainsnak.datavalue.value.id;
+
+            show_country(country_ID)
+
+
+        }
+
+    })
+
+}
+
+
+
+function show_country(country_ID) {
+
+    $.ajax({
+
+        url: "https://www.wikidata.org/w/api.php?action=wbgetclaims&format=json&entity=" + country_ID + "&property=P935&origin=*",
+
+
+        success: function (result) {
+            $("#country").empty();
+            var country_name = result.claims.P935[0].mainsnak.datavalue.value;
+            $("#country").append("Country of origin:" + country_name);
+
+
+        },
+        error: function (result) {
+            console.log("Error: ", result)
+        }
+
+    })
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+
+function ready (error, datapoints) {
+
+    d3.select("#literature").on("click", function() {
+        console.log("hello")
+        make_bubbles(error, datapoints, "literature")
+
+    })
+
+    d3.select("#philosophy").on("click", function() {
+        console.log("hello")
+        make_bubbles(error, datapoints, "philosophy")
+
+    })
+
+
+    function make_bubbles(error, datapoints, genre) {
+
+
+}*/
+
+
+/*
+
+.attr("fill", function(d) {
+    if (d.genre === "religion") {
+        return "#9FB8B1";
+    } else if (d.genre === "philosophy") {
+        return "#F67280";
+    } else if (d.genre === "law_social_sciences_education") {
+        return "#C06C84";
+    }  else if (d.genre === "applied_sciences") {
+        return "#6C5B7B";
+    }  else if (d.genre === "exact_sciences") {
+        return "#355C7D";
+    }  else if (d.genre === "art_games_sport") {
+        return "#655131";
+    } else if (d.genre === "literature") {
+        return "#357D59";
+    } else if (d.genre === "history_geography_biography") {
+        return "#BB654E";
+    }
+    return "#355C7D";
+})
+
+
+*/
