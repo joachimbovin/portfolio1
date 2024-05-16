3e934e89a018475226ea36f9429b04229c99a5c8
joachimbovin <joachimbovin@hotmail.com>
2024-05-16 11:42:20 +0200

    centered footer & added transition effect to collapsibles

--- functions.js
+++ functions.js
@@ -7,10 +7,7 @@ for (let i = 0; i < coll.length; i++) {
   coll[i].addEventListener("click", function() {
     this.classList.toggle("active");
     const content = this.nextElementSibling;
-    if (content.style.display === "block") {
-      content.style.display = "none";
-    } else {
-      content.style.display = "block";
-    }
+    content.classList.toggle("active");
+
   });
 }
